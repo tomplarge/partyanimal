3f99c4d176ee8d11725ad2118a63bb65f32af1e4
Tom Large <tomlarge@dhcp-10-105-112-162.wireless.northwestern.private>
2017-04-29 18:53:20 -0500

    more test

--- test.py
+++ test.py
@@ -6,15 +6,23 @@ data_dab = full_data(1493500380,1,True)
 data_not_dab = full_data(1493500936,1,False)
 data_dab_1 = full_data(1493500659,2,True)
 id_str = 'orientationEuler'
+#
+# plot(data_dab[id_str])
+# plot(data_not_dab[id_str])
+# plot(data_dab_1[id_str])
 
-plot()
-# data_dab[id_str] = data_dab[id_str][100:]
-# data_dab_1[id_str] = data_dab_1[id_str][100:]
-# data_not_dab[id_str] = data_not_dab[id_str][100:]
-#
-#
-# diff_combined,component = dist(data_dab[id_str],data_not_dab[id_str])
-# same_combined,component = dist(data_dab[id_str],data_dab_1[id_str])
-# diff_combined_1,component = dist(data_dab_1[id_str],data_not_dab[id_str])
-#
-# plot(diff_combined,same_combined,diff_combined_1)
+
+data_dab[id_str] = data_dab[id_str][100:]
+data_dab_1[id_str] = data_dab_1[id_str][80:]
+data_not_dab[id_str] = data_not_dab[id_str][85:]
+
+
+diff_combined,component = dist(data_dab[id_str],data_not_dab[id_str])
+same_combined,component = dist(data_dab[id_str],data_dab_1[id_str])
+diff_combined_1,component = dist(data_dab_1[id_str],data_not_dab[id_str])
+
+
+plt.plot(diff_combined,'g')
+plt.plot(same_combined,'r')
+plt.plot(diff_combined_1,'g')
+plt.show()
