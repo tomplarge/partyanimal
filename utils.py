9e7bdedf0fc13500e20faa8f9c2940ed982d9abf
Tom Large <tomlarge@dhcp-10-105-112-162.wireless.northwestern.private>
2017-04-29 17:56:48 -0500

    more utils functions

--- utils.py
+++ utils.py
@@ -12,3 +12,18 @@ def plot(data):
 
 def make_path(id_str):
     return "./data/dab1_"+id_str+"-1493500380.csv"
+
+def full_data(id_num,dataset_num,dab):
+    id_str = ['accelerometer','emg','gyro','orientation','orientationEuler']
+    data = {}
+    for i in range(len(id_str)):
+        path = "./data/"
+
+        if dab:
+            path += "dab"+str(dataset_num)+"_"
+        else:
+            path+="not_dab_"+str(id_num)+"_e"
+
+        path+=id_str[i]+"-"+str(id_num)+'.csv'
+        data[id_str[i]] = get_data(path)
+    return data
