b4a4a8c9f793b7e9242b85e94d493f561afdc6fe
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-04-29 17:55:49 -0500

    new plotter

--- get_data.py
+++ get_data.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 
 # takes in a csv file as a string and outputs an array
-def get_data(input_file):
+def get_data("data/dab1_accelerometer-1493500380.csv"):
 
     data = pd.read_csv(input_file, sep=',',header=None)
     arr = data.valuesarray
 
+    print arr
     return arr
