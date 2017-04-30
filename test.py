7d17cba41c503b266abe3e05dd3d390e54753253
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-04-29 22:09:19 -0500

    hopefully connecting it all

--- test.py
+++ test.py
@@ -1,58 +1,104 @@
 from utils import *
-from analyze import *
 import numpy
 
 dab_id_nums = [1493500380,1493500659,1493510372,1493510494]
-non_dab_id_nums = [1493500936,1493510560]
+non_dab_id_nums = [1493500936,1493510560,1493513448]
 
-data_not_dab_1 = full_data(non_dab_id_nums[0],1,False)
-data_not_dab_2 = full_data(non_dab_id_nums[1],2,False)
-data_dab_1 = full_data(dab_id_nums[0],1,True)
-data_dab_2 = full_data(dab_id_nums[1],2,True)
-data_dab_3 = full_data(dab_id_nums[2],3,True)
-data_dab_4 = full_data(dab_id_nums[3],4,True)
-
-id_str = 'orientation'
+# testdata = full_data(non_dab_id_nums[0],1,False)
+# testdata['orientationEuler'] = testdata['orientationEuler'][85:]
+# dabornotdab(testdata)
 #
-plt.plot(data_dab_3[id_str],'g')
-plt.plot(data_not_dab_2[id_str],'r')
-plt.show()
+# testdata = full_data(non_dab_id_nums[1],2,False)
+# testdata['orientationEuler'] = testdata['orientationEuler'][100:]
+# dabornotdab(testdata)
+#
+# testdata = full_data(non_dab_id_nums[2],3,False)
+# testdata['orientationEuler'] = testdata['orientationEuler'][70:]
+# dabornotdab(testdata)
 
+testdata = full_data(dab_id_nums[0],1,True)
+testdata['orientationEuler'] = testdata['orientationEuler'][105:]
+dabornotdab(testdata)
 
-data_dab_1[id_str] = data_dab_1[id_str][105:]
-data_dab_2[id_str] = data_dab_2[id_str][85:]
-data_dab_3[id_str] = data_dab_3[id_str][110:]
-data_dab_4[id_str] = data_dab_4[id_str][150:]
-data_not_dab_1[id_str] = data_not_dab_1[id_str][85:]
-data_not_dab_2[id_str] = data_not_dab_2[id_str][100:]
+testdata = full_data(dab_id_nums[1],2,True)
+testdata['orientationEuler'] = testdata['orientationEuler'][85:]
+dabornotdab(testdata)
 
-same_combined1,component = dist(data_dab_1[id_str],data_dab_2[id_str])
-same_combined2,component = dist(data_dab_1[id_str],data_dab_3[id_str])
-same_combined3,component = dist(data_dab_1[id_str],data_dab_4[id_str])
-same_combined4,component = dist(data_dab_2[id_str],data_dab_3[id_str])
-same_combined5,component = dist(data_dab_2[id_str],data_dab_4[id_str])
-same_combined6,component = dist(data_dab_3[id_str],data_dab_4[id_str])
-diff_combined1,component = dist(data_dab_1[id_str],data_not_dab_1[id_str])
-diff_combined2,component = dist(data_dab_1[id_str],data_not_dab_2[id_str])
-diff_combined3,component = dist(data_dab_2[id_str],data_not_dab_1[id_str])
-diff_combined4,component = dist(data_dab_2[id_str],data_not_dab_2[id_str])
-diff_combined5,component = dist(data_dab_3[id_str],data_not_dab_1[id_str])
-diff_combined6,component = dist(data_dab_3[id_str],data_not_dab_2[id_str])
-diff_combined7,component = dist(data_dab_4[id_str],data_not_dab_1[id_str])
-diff_combined8,component = dist(data_dab_4[id_str],data_not_dab_2[id_str])
+testdata = full_data(dab_id_nums[2],3,True)
+testdata['orientationEuler'] = testdata['orientationEuler'][110:]
+dabornotdab(testdata)
 
-plt.plot(same_combined1,'g')
-plt.plot(same_combined2,'g')
-plt.plot(same_combined3,'g')
-plt.plot(same_combined4,'g')
-plt.plot(same_combined5,'g')
-plt.plot(same_combined6,'g')
-plt.plot(diff_combined1,'r')
-plt.plot(diff_combined2,'r')
-plt.plot(diff_combined3,'r')
-plt.plot(diff_combined4,'r')
-plt.plot(diff_combined5,'r')
-plt.plot(diff_combined6,'r')
-plt.plot(diff_combined7,'r')
-plt.plot(diff_combined8,'r')
-plt.show()
+testdata = full_data(dab_id_nums[3],4,True)
+testdata['orientationEuler'] = testdata['orientationEuler'][150:]
+dabornotdab(testdata)
+#
+# data_not_dab_1 = full_data(non_dab_id_nums[0],1,False)
+# data_not_dab_2 = full_data(non_dab_id_nums[1],2,False)
+# data_not_dab_3 = full_data(non_dab_id_nums[2],3,False)
+# data_dab_1 = full_data(dab_id_nums[0],1,True)
+# data_dab_2 = full_data(dab_id_nums[1],2,True)
+# data_dab_3 = full_data(dab_id_nums[2],3,True)
+# data_dab_4 = full_data(dab_id_nums[3],4,True)
+
+# id_str = 'orientationEuler'
+#
+# plot(data_not_dab_3[id_str])
+# plot(data_dab_1[id_str])
+# plot(data_dab_2[id_str])
+# plot(data_dab_3[id_str])
+# plot(data_dab_4[id_str])
+# plt.show()
+#
+# data_dab_1[id_str] = normalize(data_dab_1[id_str][105:])
+# data_dab_2[id_str] = normalize(data_dab_2[id_str][85:])
+# data_dab_3[id_str] = normalize(data_dab_3[id_str][110:])
+# data_dab_4[id_str] = normalize(data_dab_4[id_str][150:])
+# data_not_dab_1[id_str] = normalize(data_not_dab_1[id_str][85:])
+# data_not_dab_2[id_str] = normalize(data_not_dab_2[id_str][100:])
+# data_not_dab_3[id_str] = normalize(data_not_dab_3[id_str][70:])
+
+# plot(data_not_dab_3[id_str])
+# plot(data_dab_1[id_str])
+# plot(data_dab_2[id_str])
+# plot(data_dab_3[id_str])
+# plot(data_dab_4[id_str])
+# plt.show()
+
+# same_combined1,component1 = dist(data_dab_1[id_str],data_dab_2[id_str])
+# same_combined2,component = dist(data_dab_1[id_str],data_dab_3[id_str])
+# same_combined3,component = dist(data_dab_1[id_str],data_dab_4[id_str])
+# same_combined4,component = dist(data_dab_2[id_str],data_dab_3[id_str])
+# same_combined5,component = dist(data_dab_2[id_str],data_dab_4[id_str])
+# same_combined6,component = dist(data_dab_3[id_str],data_dab_4[id_str])
+# diff_combined1,component = dist(data_dab_1[id_str],data_not_dab_1[id_str])
+# diff_combined2,component = dist(data_dab_1[id_str],data_not_dab_2[id_str])
+# diff_combined3,component2 = dist(data_dab_1[id_str],data_not_dab_3[id_str])
+# diff_combined4,component = dist(data_dab_2[id_str],data_not_dab_1[id_str])
+# diff_combined5,component = dist(data_dab_2[id_str],data_not_dab_2[id_str])
+# diff_combined6,component = dist(data_dab_2[id_str],data_not_dab_3[id_str])
+# diff_combined7,component = dist(data_dab_3[id_str],data_not_dab_1[id_str])
+# diff_combined8,component = dist(data_dab_3[id_str],data_not_dab_2[id_str])
+# diff_combined9,component = dist(data_dab_3[id_str],data_not_dab_3[id_str])
+# diff_combined10,component = dist(data_dab_4[id_str],data_not_dab_1[id_str])
+# diff_combined11,component = dist(data_dab_4[id_str],data_not_dab_2[id_str])
+# diff_combined12,component = dist(data_dab_4[id_str],data_not_dab_3[id_str])
+
+# plt.plot(same_combined1,'g')
+# plt.plot(same_combined2,'g')
+# plt.plot(same_combined3,'g')
+# plt.plot(same_combined4,'g')
+# plt.plot(same_combined5,'g')
+# plt.plot(same_combined6,'g')
+# plt.plot(diff_combined1,'r')
+# plt.plot(diff_combined2,'r')
+# #plt.plot(diff_combined3,'r')
+# plt.plot(diff_combined4,'r')
+# plt.plot(diff_combined5,'r')
+# #plt.plot(diff_combined6,'r')
+# plt.plot(diff_combined7,'r')
+# plt.plot(diff_combined8,'r')
+# #plt.plot(diff_combined9,'r')
+# plt.plot(diff_combined10,'r')
+# plt.plot(diff_combined11,'r')
+# #plt.plot(diff_combined12,'r')
+#plt.show()
--- utils.py
+++ utils.py
@@ -6,6 +6,9 @@ import pandas as pd
 import numpy as np
 from bokeh.palettes import Spectral11
 from bokeh.plotting import figure, show, output_file
+import scipy
+import scipy.spatial
+import os
 
 def get_data(path):
     data = genfromtxt(path, delimiter=',')
@@ -51,3 +54,133 @@ def full_data(id_num,dataset_num,dab):
         data[id_str[i]] = get_data(path)
 
     return data
+
+def normalize(v):
+    for i in range(v.shape[1]):
+        v[:,i] /= np.max(v[:,i])
+    return v
+
+def dist(x,y,metric='euclidean'):
+    #computes the absolute distance between two time sample multi-field data vectors
+    #x: first vector (numpy array)
+    #y: second vector (numpy array)
+    #metric: distance metric to use (string)
+
+    #indexing the vectors: x[time_idx][field]
+
+    #indexing output:
+    #combined[t] = distance between the n-dimensional points y[t] and x[t]
+    #component[t,n] = absolute distance between the component values y[t,n] and x[t,n]
+
+    time_points_x = x.shape[0]
+    time_points_y = y.shape[0]
+    time_points = np.minimum(time_points_x,time_points_y)
+
+    if x.shape[1] != y.shape[1]:
+        print "Data incorrectly formatted"
+
+    combined_distance = np.zeros((time_points,))
+    component_distance = np.zeros((time_points,x.shape[1]))
+
+    for t in range(time_points):
+        if metric=='euclidean':
+            combined_distance[t] = scipy.spatial.distance.euclidean(y[t],x[t])
+
+
+        elif metric == 'cityblock':
+            combined_distance[t] = scipy.spatial.distance.cityblock(y[t],x[t])
+
+        elif metric == 'cosine':
+            combined_distance[t] = scipy.spatial.distance.cosine(y[t],x[t])
+
+        for n in range(component_distance.shape[1]):
+            component_distance[t,n] = np.abs(y[t,n]-x[t,n])
+
+    return combined_distance,component_distance
+
+def one_component_flat(oE_data):
+    #only 3 components
+    flat = [False,False,False]
+    flat_ctr = 0
+    thresh = 0.7
+    for i in range(3):
+        variance = np.abs(np.amax(oE_data[:,i]) - np.amin(oE_data[:,i]))
+        #print variance
+        if variance > thresh:
+            flat[i] = False
+        else:
+            flat[i] = True
+            flat_ctr += 1
+    if flat_ctr == 0:
+        return False
+    else:
+        return True
+
+
+def dabornotdab():
+    path = "./data/test.csv"
+    ratings = [0,0,0,0,0,0]
+    rating = 4
+    ratings[0] = "I honestly don't know if you just sneezed or tried to dance."
+    ratings[1] = "This isn't the fucking chicken dance."
+    ratings[2] = "I can tell what you're trying to do, but if anyone asks we don't know each other, ok?"
+    ratings[3] = "Well... not terrible. Practice that form a bit more and you'll be in good shape."
+    ratings[4] = "You don't need a dabby you dabstar."
+    ratings[5] = "Literally. Perfect."
+
+    id_str = 'orientationEuler'
+    #oE_data = input_data[id_str]
+    oE_data = get_data(path)
+    oE_data_norm = normalize(oE_data)
+    oE_data = np.nan_to_num(oE_data)
+    oE_data_norm = np.nan_to_num(oE_data_norm)
+
+    maxmin_thresh = 1.5
+    diff_thresh = 5
+
+    if one_component_flat(oE_data) == False:
+        #print "failed flat component analysis"
+        rating -= 1
+
+    data_dab = [0,0,0,0]
+    dab_id_nums = [1493500380,1493500659,1493510372,1493510494]
+
+    data_dab[0] = full_data(dab_id_nums[0],1,True)
+    data_dab[1] = full_data(dab_id_nums[1],2,True)
+    data_dab[2] = full_data(dab_id_nums[2],3,True)
+    data_dab[3] = full_data(dab_id_nums[3],4,True)
+
+    data_dab[0] = normalize(data_dab[0][id_str][105:])
+    data_dab[1] = normalize(data_dab[1][id_str][85:])
+    data_dab[2] = normalize(data_dab[2][id_str][110:])
+    data_dab[3] = normalize(data_dab[3][id_str][150:])
+
+    differences = [0,0,0,0]
+
+    for i in range(4):
+        differences[i],component = dist(data_dab[i],oE_data_norm)
+        if np.amax(differences[i]) > diff_thresh:
+            #print "failed 5 threshold analysis"
+            rating -= 1
+            break
+
+    for i in range(4):
+        differences[i],component = dist(data_dab[i],oE_data_norm)
+        if np.amin(differences[i]) > diff_thresh/2:
+            #print "failed minimum threshold analysis"
+            rating -= 1
+            break
+
+    for i in range(4):
+        if np.amax(differences[i]) - np.amin(differences[i]) > maxmin_thresh:
+            #print "failed max-min analysis"
+            rating -= 1
+            break
+
+    for i in range(4):
+        if np.amax(differences[i]) < 0.5:
+            rating = 5
+
+    say_string = "say "+"\"" + ratings[rating]+"\""
+    #print say_string
+    os.system(say_string)
