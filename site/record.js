72382c8449a0d613a69143707ffe920cd84bf700
sarahjwong <sarahwong2019@u.northwestern.edu>
2017-04-29 22:08:25 -0500

    connecting?

--- site/record.js
+++ site/record.js
@@ -1,7 +1,14 @@
 function recordDab() {
   var answer = confirm("Ready to see Who's Your Dabby?");
   if (answer) {
-    
+    $.ajax({
+      url: "../whosyourdabby.py"
+      success: function(response) {
+      document.getElementById(title).innerHTML = response;
+      }
+
+
+    })
   }
 
 
