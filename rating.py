2ac36d25325d81251399cc77c63d37fc3eee867f
rawitscher <lindsayrawitscher2017@u.northwestern.edu>
2017-04-29 19:35:49 -0500

    rating fixed

--- rating.py
+++ rating.py
@@ -26,35 +26,52 @@ def top_ten_orient_gyro():
 # Generate response for dab
 def in_depth_rate_dab(orient_gyro, accel):
     response = "In terms of your timing: "
-    if orient_accel < 10:
-        response += below10_accel
-    elif orient_accel < 25:
-        response += onequarter_accel
-    elif orient_accel < 50:
-        response += onehalf_accel
-    elif orient_accel < 75:
-        response += threequarters_accel
+    if accel < 10:
+        response += below10_accel()
+    elif accel < 25:
+        response += onequarter_accel()
+    elif accel < 50:
+        response += onehalf_accel()
+    elif accel < 75:
+        response += threequarters_accel()
     else:
-        response += top_ten_accel
-    response += "and as for your dabbing: "
+        response += top_ten_accel()
+    response += " As for your dabbing: "
     if orient_gyro < 10:
-        response += below10_orient_gyro
+        response += below10_orient_gyro()
     elif orient_gyro < 25:
-        response += onequarter_orient_gyro
+        response += onequarter_orient_gyro()
     elif orient_gyro < 50:
-        response += onehalf_orient_gyro
+        response += onehalf_orient_gyro()
     elif orient_gyro < 75:
-        response += threequarters_orient_gyro
+        response += threequarters_orient_gyro()
     else:
-        response += top_ten_orient_gyro
-        }
-
+        response += top_ten_orient_gyro()
+    return response
 
 def rate_dab(orient_gyro, accel):
+    print "orient_gyro: "+str(orient_gyro)
+    print "accel: "+str(accel)
+    print "rating: "
     if orient_gyro + accel > 180:
         response = "You fucking rock, dabstar."
     elif orient_gyro + accel < 50:
         response = "Honestly, get the fuck out the club."
     else:
-        in_depth_rate_dab(orient_gyro, accel)
+        response = in_depth_rate_dab(orient_gyro, accel)
+    print response
     return response
+
+#### Testing these bad boys out ####
+
+rate_dab(34,86)
+rate_dab(55,100)
+rate_dab(2,1)
+rate_dab(66,28)
+rate_dab(90,30)
+rate_dab(80,95)
+
+import pyttsx
+engine = pyttsx.init()
+engine.say('Good morning.')
+engine.runAndWait()
